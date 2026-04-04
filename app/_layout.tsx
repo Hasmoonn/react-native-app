c3f3985114849512d866c55f0a19a8526a2082df
Hasmoonn <mohamedhasmoon175@gmail.com>
2026-04-04 22:36:54 +0530

    error correction from coderabbit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
 
+SplashScreen.preventAutoHideAsync(); // Keep the splash screen visible while we fetch resources
+
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
     'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
